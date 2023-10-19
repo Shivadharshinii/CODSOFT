9f3418974a12615763e8805a0349be783ab0350f
Shiva Dharshini S <146826218+Shivadharshinii@users.noreply.github.com>
2023-10-19 19:38:18 +0530

    Update GradeCalculator.java

--- GradeCalculator.java
+++ GradeCalculator.java
@@ -39,3 +39,38 @@ public class GradeCalculator{
          System.out.println("F");
    }
 }
+//alternate
+import java.util.Scanner;
+public class GradeCalculator {
+public static void main(String[]args){
+   float avg;
+	float total;
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter the cloudcomputing mark:");
+	int a=sc.nextInt();
+	System.out.println("Enter the web programming mark:");
+	int b=sc.nextInt();
+	System.out.println("Enter the english mark:");
+	int c=sc.nextInt();
+	System.out.println("Enter the computerarchitecture mark:");
+	int d=sc.nextInt();
+	System.out.println("Enter the java programming mark:");
+	int e=sc.nextInt();
+	System.out.println("Enter the DBMS mark:");
+	int f=sc.nextInt();
+	total=a+b+c+d+e+f;
+	System.out.println("Total:"+total);
+	avg=total/6;
+	System.out.println("Average mark:"+String.format("%.2f",avg));
+	if(avg>90)
+		System.out.println("Grade:O");
+    else if(avg>80&&avg<=90)
+    	System.out.println("Grade:A+");
+    else if(avg>70&&avg<=80)
+    	System.out.println("Grade:A");
+    else if(avg>60&&avg<=70)
+    	System.out.println("Grade:B+");
+    else 
+    	System.out.println("Grade:B");
+}
+}
